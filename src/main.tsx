5fa7f1167a6752a1a4175ed52415fe92ea94004d
fengjutian <2640111081@qq.com>
2024-12-12 08:37:39 +0800

    feat: 保存

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.tsx';
 import AddData from './addData/Books.tsx';
+import BodyIcon from './BodyIcon.tsx';
+import Home from './home/Home.tsx';
+import "@arco-design/web-react/dist/css/arco.css";
+import 'tdesign-react/es/style/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={ <StrictMode><App /></StrictMode>} />
       <Route path="/books" element={<AddData/>}/>
+      <Route path="/body-icon" element={<BodyIcon/>}/>
+      <Route path="/home" element={<Home/>}/>
     </Routes>
   </BrowserRouter>
 )
